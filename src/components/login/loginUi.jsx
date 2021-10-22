08a5d31531bcd31821f3cae876b88ea34a29b863
ankit Bathla <ankit.bathla@zopsmart.com>
2021-10-22 17:16:39 +0530

    feat:bb

--- src/components/login/loginUi.jsx
+++ src/components/login/loginUi.jsx
@@ -40,7 +40,9 @@ const LoginUI = (props) => {
                             <div>
                                 {translate("Do_not_have_an_account")}
                                 <span>
-                                    <Link to={`/signUp?${link}`}>
+                                    <Link
+                                        to={`/signUp?state=hKFo2SBYT0tHajJaaEZoYXFwMmJuNGdUYzZjZW1DZUlTNlRCcaFupWxvZ2luo3RpZNkgWVJOd0JkWW84clJIcXRaazV1amxxczBDLXFCZTZKUDOjY2lk2SA1ckF1ODB4QXBhRUZ5R3hUQXNXQkVsNUZpTlZZZzFJTA&client=5rAu80xApaEFyGxTAsWBEl5FiNVYg1IL&protocol=oauth2&redirect_uri=http%3A%2F%2Flocalhost%3A3000&fragment=culture%3Dfr-ca%26aff_id%3D0&scope=openid%20profile%20email&response_type=code&response_mode=query&nonce=ZDMzNFlwYn5zTlBVa2ptdjJvTEdSMEF3MEE4c09mRU4tSnZFUE40ZmNMUw%3D%3D&code_challenge=_xgtl-6_B9eMvRZ4MxjS7jjFtPT-5WRJYQofjyvI8Ao&code_challenge_method=S256&auth0Client=eyJuYW1lIjoiYXV0aDAtcmVhY3QiLCJ2ZXJzaW9uIjoiMS44LjAifQ%3D%3D#culture=fr-ca&aff_id=0`}
+                                    >
                                         {" "}
                                         Create one now
                                     </Link>
