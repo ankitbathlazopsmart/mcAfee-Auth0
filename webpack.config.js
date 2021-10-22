7a9672f0195bd36008525d29408a81f1a23b9fd7
ankit Bathla <ankit.bathla@zopsmart.com>
2021-10-22 17:29:07 +0530

    feat:bb

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         main: lModules,
     },
     output: {
-        filename: "bundle_24.min.js",
+        filename: "bundle_25.min.js",
         path: path.resolve(__dirname, "dist"),
     },
     module: {
